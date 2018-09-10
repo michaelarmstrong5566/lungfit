acb273ffdfb0919a50a896a47d022076ab6d5b76
Michael Armstrong <mcarmstrong@gmail.com>
2018-09-10 06:27:12 -0700

    modified script.js to log male radio group value in the console

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 function myFunction() {
-      alert("You are smart");
+      alert("You pressed Submit!");
 }
 
 function calculateDistanceWalked(){
-    var x = document.getElementById("m").value;
-    alert(x);
+    var m = document.getElementById("m").value;
+    var f = document.getElementById("f").value;
+    var h = document.getElementById("h").value;
+
+    console.log("m: "+ m);
 
 } 
 
