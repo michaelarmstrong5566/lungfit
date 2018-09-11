69d0888e079738c63a84dae2fa9d190135249ddc
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-11 11:57:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 function putFormElementsInArray(){
-	var x = document.getElementById("6mwd_form");
-	var i;
-	var six_mwd_array = ["gender", "height", "age", "laps", "distance_per_lap"];
-
-	for(i=0; i<x.length(); i++){
-
-	}
-	console.log("The number of form elements= " + x.length());
 
 }
 
-
 function calculateDistanceWalked(){
-  var x = document.getElementById("6mwd_form");
-  
-  alert("You are about to calculate the distance walked: Gender= " + x);
- 
-  
+   
+  alert("You are about to calculate the distance walked:");
+
 } 
 
 function calculatePrecentPredicted(){
