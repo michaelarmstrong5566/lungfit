f22e0cb695c33321b66361de7f754506694fd382
Michael Armstrong <mcarmstrong@gmail.com>
2018-09-11 06:22:27 -0700

    modified script.js and index.html

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function putFormElementsInArray(){
+	var x = document.getElementById("6mwd_form");
+	var i;
+	var 6mwd_array = [gender, height, age, laps, distance_per_lap];
+
+	for(i=0; i<x.length; i++){
+
+	}
+	console.log("The number of form elements= " x.lenth);
+
+}
+
 
 function calculateDistanceWalked(){
   var x = document.getElementsByName("Gender").value;
