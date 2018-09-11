016a5c0d58082fc0e62b9e1a7118ea3fc16b84a9
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-10 21:01:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ function myFunction() {
 }
 
 function calculateDistanceWalked(){
-    var m = document.getElementById("m").value;
-    var f = document.getElementById("f").value;
+  
     var h = document.getElementById("h").value;
     var a = document.getElementById("a").value;
     var l = document.getElementById("l").value;
     var dpl = document.getElementById("dpl").value;
       
-    console.log("m: "+ m);
-    console.log("f: "+ f);
     console.log("h: "+ h);
     console.log("l: "+ l);
     console.log("dpl: "+ dpl);
