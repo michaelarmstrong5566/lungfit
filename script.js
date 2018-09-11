59307ddfbd294ed404da7184e47ec019772f05f9
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-10 21:07:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 
-function myFunction() {
-      alert("You pressed Submit!");
-}
-
 function calculateDistanceWalked(){
-  
-    var h = document.getElementById("h").value;
-    var a = document.getElementById("a").value;
-    var l = document.getElementById("l").value;
-    var dpl = document.getElementById("dpl").value;
-      
-    console.log("h: "+ h);
-    console.log("l: "+ l);
-    console.log("dpl: "+ dpl);
-      
-
+  alert("You are about to calculate the distance walked");
 } 
 
 function calculatePrecentPredicted(){
