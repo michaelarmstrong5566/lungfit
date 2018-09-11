a7d4e3abf20ab3b4bf747e099e9a8fb073174dae
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-11 11:14:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function putFormElementsInArray(){
 	var x = document.getElementById("6mwd_form");
 	var i;
-	var 6mwd_array = ["gender", "height", "age", "laps", "distance_per_lap"];
+	var six_mwd_array = ["gender", "height", "age", "laps", "distance_per_lap"];
 
-	for(i=0; i<x.length; i++){
+	for(i=0; i<x.length(); i++){
 
 	}
-	console.log("The number of form elements= " x.lenth);
+	console.log("The number of form elements= " x.length());
 
 }
 
