237db45d9865b803727b035a1fb6e3ac3c2b7929
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-10 23:24:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function calculateDistanceWalked(){
-  var x = document.getElementsByName("Gender").value;
+  var x = document.getElementsByName("Gender");
   
   alert("You are about to calculate the distance walked: Gender= " + x);
  
