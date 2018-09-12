20a481fb57210bd3343c2193da853698d71a135e
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-12 09:02:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function calculateDistanceWalked(){
   var gender_select = document.getElementById("g_select");
-  alert(gender_select.value);
+  alert("gender_select:" + gender_select.value);
   
 
   var gender = document.getElementById("g");
-  alert(gender.value);
+  alert("gender:" + gender.value);
 
+  alert("gender_select + gender: " + (gender_select.value + gender.value));
  
 
 
