12d5d8eadbd6d0a27c9a6870db83a33f8be0f973
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-12 11:46:29 -0700

    Update script.js
    get elements and store in a variable

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function calculateDistanceWalked(){
-  var gender_select = document.getElementById("g_select");
-  alert("gender_select:" + gender_select.value);
-  
 
   var gender = document.getElementById("g");
+  var height = document.getElementById("h");
+  var age = document.getElementById("a");
+  var laps = document.getElementById("l");
+  var distance_per_lap = document.getElementById("dpl");
+  
   alert("gender:" + gender.value);
-
-  alert("gender_select + gender: " + (parseInt(gender_select.value) + parseInt(gender.value)));
+  alert("height:" + height.value);
+  alert("age:" + age.value);
+  alert("laps:" + laps.value);
+  alert("distance/lap:" + distance_per_lap.value);
+ 
+  //alert("gender_select + gender: " + (parseInt(gender_select.value) + parseInt(gender.value)));
  
 
 
