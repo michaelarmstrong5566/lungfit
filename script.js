7f74c8b2eae4c4388a1fa28e70466dbe79d36284
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-13 11:30:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 function calculatePredictedWalkDistance(){
 
+  //get gender, height, age, laps, distance/lap
   var gender = document.getElementById("g");
   var height = document.getElementById("h");
   var age = document.getElementById("a");
+
+  //calculate predicted distance
+  var predicted_distance = 218+(5.14*parseInt(height.value)-5.32*parseInt(age.value))-1.8*parseInt(height.value)+51.31*parseInt(gender.value);
+  
+  //output the calcualted predicted walk distance
+  document.getElementById("predicted_walk_distance").innerHTML = "Predicted Walk Distance: " + parseInt(predicted_distance) +"m";
+  
+  return parseInt(predicted_distance);
+} 
+
+function calculateDistanceWalked(){
   var laps = document.getElementById("l");
   var distance_per_lap = document.getElementById("dpl");
   
-  var predicted_distance = 218+(5.14*parseInt(height.value)-5.32*parseInt(age.value))-1.8*parseInt(height.value)+51.31*parseInt(gender.value);
+  //calculate distance walked
+  var distance_walked = parseInt(l.value)*parseInt(dpl.value);
   
-  document.getElementById("predicted_walk_distance").innerHTML = "Predicted Walk Distance: " + parseInt(predicted_distance);
+  //output the distance walked
+  document.getElementById("distance_walked").innerHTML = "Distance Walked: " + parseInt(distance_walked) +"m";
   
-  //alert("gender_select + gender: " + (parseInt(gender_select.value) + parseInt(gender.value)));
-   
-
-
-} 
+  return parseInt(distance_walked); 
+}
 
 function calculatePrecentPredicted(){
 
