ce3cf77f1114f1f8fd07c6bfc91f26b09431313f
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-12 21:20:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function calculateDistanceWalked(){
   var laps = document.getElementById("l");
   var distance_per_lap = document.getElementById("dpl");
   
+  return 218+(5.14*parseInt(gender.value)-5.32*parseInt(age.value))-1.8*parseInt(height.value)+51.31*parseInt(gender.value);
+  
   //alert("gender_select + gender: " + (parseInt(gender_select.value) + parseInt(gender.value)));
  
 
