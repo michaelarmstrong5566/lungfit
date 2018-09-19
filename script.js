26dc43379eb0371f0438b9a6f095e1eadc14bca7
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-19 15:26:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -45,8 +45,7 @@ function testing(){
 }
         
     //calculate predicted distance
-     var predicted_distance = 218+
-                              (5.14*height.value)
+     var predicted_distance = 218+(5.14*height.value);
      //                         -5.32*parseInt(age.value))
      //                         -1.8*parseInt(height.value)
      //                         +51.31*parseInt(gender.value);
