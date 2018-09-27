132ebc59e5bb5c308c7615c0bd218d54b6d68834
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-09-27 14:22:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function testing(){
     
     var fev1 = form[6];
     console.log(fev1.value);
-
+    fev1 = fev1.value;
         
     //calculate predicted distance, distance walked and percent predicted
     var predicted_distance = 218+(5.14*height.value - 5.32*age.value) - 1.8*height.value + 51.31*gender.value;
@@ -41,7 +41,7 @@ function testing(){
     document.getElementById("predicted_walk_distance").innerHTML = "Predicted Walk Distance: " + predicted_distance.toFixed(0) +" m";
     document.getElementById("distance_walked").innerHTML = "Distance Walked: " + distance_walked.toFixed(0) +" m";
     document.getElementById("percent_predicted").innerHTML = "Percent predicted: "+ percent_predicted.toFixed(0) +" %";
-    document.getElementById("percent_predicted").innerHTML = "Lung Age: "+ fev1.value().toFixed(2) +" years";
+    document.getElementById("percent_predicted").innerHTML = "Lung Age: "+ fev1 +" years";
     
 } 
 
