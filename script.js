802b77edfa13504f7d20e953fcae5de6f31aa2a3
michaelarmstrong5566 <mcarmstrong@gmail.com>
2018-10-10 09:35:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -42,13 +42,16 @@ function testing(){
  
     //calculate D
     var D = calculateD(C, gender.value);
+ 
+    //calcualte Lung Age
+    var LungAge = calculateLungAge(D, gender.value);
 
      
     //output the calculated predicted walk distance, distance walked  and percent predicted
     document.getElementById("predicted_walk_distance").innerHTML = "Predicted Walk Distance: " + predicted_distance.toFixed(0) +" m";
     document.getElementById("distance_walked").innerHTML = "Distance Walked: " + distance_walked.toFixed(0) +" m";
     document.getElementById("percent_predicted").innerHTML = "Percent predicted: "+ percent_predicted.toFixed(0) +" %";
-    document.getElementById("lung_age").innerHTML = "Lung Age: "+ fev1.value +" years";
+    document.getElementById("lung_age").innerHTML = "Lung Age: "+ LungAge +" years";
     document.getElementById("C").innerHTML = "C: "+ C;
     document.getElementById("D").innerHTML = "D: "+ D;
     
@@ -78,6 +81,18 @@ function testing(){
          } 
         }
 
+ function calculateLungAgeD(D,G){
+         if (G == 1){
+         
+         return parseFloat(D);
+          
+         }
+         else {
+          
+         return parseFloat(D);
+         
+         } 
+        }
 
 
 
